68917ca8fe79fd80b5b452cbef7770078baab5cf
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-03-21 10:02:46 +0100

    Add files via upload

---
+++ dropDownOceania.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleDropdownOceania").addEventListener("click", function() {
+    var dropdown_oceania = document.getElementById("dropdownContentOceania");
+    dropdown_oceania.style.display = (dropdown_oceania.style.display === "none" || dropdown_oceania.style.display === "") ? "block" : "none";
+});
